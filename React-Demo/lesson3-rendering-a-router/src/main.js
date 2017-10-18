bb516859532c7d8f66dc16dc2006d7cd9d7acb8f
angus_qing168@163.com <angus_qing168@163.com>
2017-10-18 16:45:31 +0800

    router hasHistory or browserHistory compare

--- React-Demo/lesson3-rendering-a-router/src/main.js
+++ React-Demo/lesson3-rendering-a-router/src/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, hashHistory, browserHistory } from 'react-router';
 import App from './components/app.jsx';
 import About from './components/about.jsx';
 import Repos from './components/repos.jsx';
@@ -8,7 +8,13 @@ import Repos from './components/repos.jsx';
 var container = document.getElementById('app');
 
 ReactDOM.render((
-    <Router history={hashHistory}>
+    /* comment: 
+        Hash history works without configuring your server, 
+        so if you're just getting started, go ahead and use it. 
+        But, we don't recommend using it in production, 
+        every web app should aspire to use browserHistory */
+    //<Router history={hashHistory}>
+    <Router history={browserHistory}> 
         <Route path="/" component={App}/>
         {/* add the routes here */}
         <Route path="/repos" component={Repos}/>
